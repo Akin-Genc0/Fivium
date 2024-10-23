0a171acab6eb7a18004663822fa530b3ec6e17f3
Akin-Genc0 <akingenc212@gmail.com>
2024-10-23 11:06:23 +0100

    Added responsive 404 error NoPage component and page

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,15 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Careers from './pages/Careers'; 
+import Nopage from './pages/nopage';    // 404 page or "not found" page
 
 function App() {
-
-
   return (
-    <>
-  <Careers/>
-
-    </>
-  )
+    <Router>
+      <Routes>
+       
+        <Route path="/" element={<Careers />} />
+        <Route path="*" element={<Nopage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
